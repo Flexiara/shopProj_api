5f230e121e1905d1d0cb4b8291ba0de9538b2481
git-admin <kumosolutionn@gmail.com>
2023-09-20 15:43:48 +0630

    add hashed password

--- src/Controller/register.js
+++ src/Controller/register.js
@@ -1,28 +1,49 @@
 const User = require("../Models/user.modal")
+const bcrypt = require ("bcrypt")
+const saltRounds = 10; // Adjust the number of rounds as needed
 
-const loginAcc =async (req, res, next) => {
-    userName= req.userName
-    password = req.password
+const loginAcc = async (req, res, next) => {
+    email = req.body.email
+    password = req.body.password
     try {
-
-    } 
-    catch(err){
+        result = await User.findOne({ email: email })
+        if (result) {
+            passwordPass = bcrypt.compareSync(password, result.password)
+            console.log(passwordPass);
+            if (passwordPass) {
+                res.json(result)
+            } else {
+                throw ("Wrong Password")
+            }
+        } else {
+            throw ("Invalid Email")
+        }
+    }
+    catch (err) {
         next(err)
     }
 }
 
-const registerAcc = async(req, res, next) => {   console.log(">>>tryryrtyrty>>");
-   try {
-  const result = await User.save(req)
-//   res.json(result)
-   }
-   catch(err){
-    next(err)
-   }
+const registerAcc = async (req, res, next) => {
+    try {
+        const data = new User(req.body) 
+        existEmail = await User.findOne({ email: data.email })
+        if (!existEmail) {
+            const hashPassword = bcrypt.hashSync(data.password, saltRounds);
+            data.password = hashPassword
+            const result = await data.save()
+            res.json(result)
+        } else {
+            throw ("Email Aleady Used!")
+        }     
+    }
+    catch (err) {
+        next(err)
+    }
 }
 
 
 
 module.exports = {
-    registerAcc
+    registerAcc, loginAcc
 }
